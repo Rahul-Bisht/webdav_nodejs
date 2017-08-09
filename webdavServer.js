72e577b7e0be241ae47583b535cf1bbe7a1a511b
Bisht <rahulbisht140@gmail.com>
2017-08-09 14:31:26 +0530

    added unknown method.

--- webdavServer.js
+++ webdavServer.js
@@ -2,20 +2,37 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var webdav_server_1 = require("webdav-server");
 var fs = require("fs");
-var server = new webdav_server_1.v2.WebDAVServer({ port: 4005, requireAuthentification: false });
-var ctx = server.createExternalContext();
-var subTree = { "testFileSystem": { "test.txt": webdav_server_1.v2.ResourceType.File } };
-server.method("get", {
+// add user list to user manager
+var userManager = new webdav_server_1.v2.SimpleUserManager();
+userManager.addUser("test", "123", true);
+userManager.addUser("test2", "123", false);
+var server = new webdav_server_1.v2.WebDAVServer({ port: 4005, requireAuthentification: true, httpAuthentication: new webdav_server_1.v2.HTTPDigestAuthentication(userManager, 'default realm', 1) });
+server.setFileSystem("/testFileSystem", new webdav_server_1.v2.PhysicalFileSystem("/testFileSystem"), function (success) {
+    console.log(">>Physical file system has been mounted");
+});
+//unknown http method
+server.onUnknownMethod({
     unchunked: function (ctx, data) {
     }, chunked: function (ctx, inputStream) {
+        ctx.response.end("unknown method");
+    }, isValidFor: function (ctx, rt) {
+        return false;
+    }
+});
+server.method("get", {
+    unchunked: function (ctx, data) {
+        debugger;
+        ctx.response.end();
+    }, chunked: function (ctx, inputStream) {
+        debugger;
         var url = __dirname + '/' + ctx.request.url;
         fs.readFile(url, function (e, data) {
             ctx.response.end(data);
         });
-    }, isValidFor: function (ctx, rt) {
-        ctx.askForAuthentication(true, function (err) {
-        });
-        return true;
+    },
+    isValidFor: function (ctx, rtype) {
+        debugger;
+        return false;
     }
 });
 server.start(function () { console.log('webdav server is running...'); });
--- webdavServer.ts
+++ webdavServer.ts
@@ -1,24 +1,43 @@
 import { v2 as webdav } from 'webdav-server';
 import * as fs from 'fs';
 
-const server = new webdav.WebDAVServer({ port: 4005, requireAuthentification: false });
-const ctx = server.createExternalContext();
+// add user list to user manager
+let userManager = new webdav.SimpleUserManager();
+userManager.addUser("test", "123", true);
+userManager.addUser("test2", "123", false);
 
-let subTree = { "testFileSystem": { "test.txt": webdav.ResourceType.File } };
+const server = new webdav.WebDAVServer({ port: 4005, requireAuthentification: true, httpAuthentication: new webdav.HTTPDigestAuthentication(userManager, 'default realm', 1) });
 
-server.method("get", {
+
+server.setFileSystem("/testFileSystem", new webdav.PhysicalFileSystem("/testFileSystem"), (success) => {
+    console.log(">>Physical file system has been mounted");
+});
+
+//unknown http method
+server.onUnknownMethod({
     unchunked: (ctx, data) => {
 
     }, chunked: (ctx, inputStream) => {
-        let url=__dirname+'/'+ ctx.request.url;
-        fs.readFile(url,(e,data)=>{
+        ctx.response.end("unknown method");
+    }, isValidFor: (ctx, rt) => {
+        return false;
+    }
+});
+
+server.method("get", {
+    unchunked: (ctx, data) => {
+        debugger;
+        ctx.response.end();
+    }, chunked: (ctx, inputStream) => {
+        debugger;
+        let url = __dirname + '/' + ctx.request.url;
+        fs.readFile(url, (e, data) => {
             ctx.response.end(data);
         });
-     
-    }, isValidFor: (ctx, rt) => {
-        ctx.askForAuthentication(true,(err)=>{
-        })
-        return true;
-    }
-}); 
- server.start(() => { console.log('webdav server is running...') });
+    },
+    isValidFor:(ctx,rtype)=>{
+        debugger;
+        return false; }
+});
+
+server.start(() => { console.log('webdav server is running...') });
